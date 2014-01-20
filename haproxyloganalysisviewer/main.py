682ecb116fabad43d07a0d90b63a49dda97da77f
Bogdan Girman <bogdan.girman@gmail.com>
2014-01-20 15:28:42 +0200

    Add filter parameters support

--- haproxyloganalysisviewer/main.py
+++ haproxyloganalysisviewer/main.py
@@ -3,6 +3,7 @@ import tornado.web
 import tornado.template
 from haproxy.haproxy_logfile import HaproxyLogFile
 from haproxy.main import VALID_FILTERS
+from haproxy import filters as modFilters
 import StringIO
 
 log_file = HaproxyLogFile()
@@ -10,7 +11,8 @@ log_file = HaproxyLogFile()
 class UploadHandler(tornado.web.RequestHandler):
     """ Upload files """
     def get(self):
-        self.render("templates/upload.html", log_file=log_file, filters=VALID_FILTERS)
+        filters = [eval('modFilters.filter_' + i) for i in VALID_FILTERS]
+        self.render("templates/upload.html", log_file=log_file, filters=filters)
 
     def post(self):
         global log_file
@@ -18,7 +20,25 @@ class UploadHandler(tornado.web.RequestHandler):
             log_file_data = StringIO.StringIO(self.request.files.get('logfile')[0]['body'])
             if log_file_data is not None:
                 log_file.parse_data(log_file_data)
-        self.render("templates/upload.html", log_file=log_file)
+        filters = []
+        for filter in self.request.arguments.keys():
+            if ''.join(self.request.arguments[filter]):
+                filters.append({
+                    'name':filter.split('.')[0],
+                    'arguments':dict([(i,self.request.arguments[filter]) for i in filter.split('.')[1:]]),
+                    }
+                    )
+        for filter in filters:
+            log_file = log_file.filter(
+                eval(
+                    'modFilters.' + filter['name'] + '(' + ','.join(
+                        [i+'="'+''.join(filter['arguments'][i])+'"' for i in filter['arguments'] if ''.join(filter['arguments'][i])]
+                        ) + ')'
+                    )
+                )
+        
+        filters = [eval('modFilters.filter_' + i) for i in VALID_FILTERS]
+        self.render("templates/upload.html", log_file=log_file, filters=filters)
 
 class RunCommandHandler(tornado.web.RequestHandler):
     """ Upload files """
